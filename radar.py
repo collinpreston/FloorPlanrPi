021fd6098256f1765efe82230f444a707d18f2a0
Collin Preston <collin_preston@yahoo.com>
2020-05-02 08:30:12 -0400

    Most recent code which sends each angle as it's received to the screen

--- radar.py
+++ radar.py
@@ -7,11 +7,16 @@ import numpy as np
 ser = serial.Serial ("/dev/serial0", baudrate=230400)
 
 def init():
-    try:
-        distance_list = [None] * 360
-        unique_values = 0;
-        while unique_values < 360:
-            result = ser.read(42)
+    r = np.arange(1, 361, 1)
+    theta = np.radians(r)
+    fig = plt.figure()
+    ax = fig.add_subplot(111, polar=True)
+    distance_list = [None] * 360
+    total = 0
+
+    while True:
+        try:
+            result = ser.read(42)  
             if (result[-1] == result[-2]):
                 rpm = result[3]*256 + result[2]
                 #print("RPM" + str(rpm))
@@ -19,32 +24,27 @@ def init():
                 for x in range(6):
                     angle = base_angle + x
                     distance = result[((6*(x+1))+1)]*256 + result[((6*(x+1)))]
-                    if distance_list[angle] == None:
-                        unique_values += 1
+                    if distance_list[angle] != distance:
                         if distance > 0:
                            distance_list[angle] = distance
+                           total += 1
                         else:
                             distance_list[angle] = 4200
-        return distance_list
-    except IndexError:
-        ser.write(b'e')
-        print('Stopped! Out of sync.')
+                            total += 1
+            print(total)
+            if total >= 360:
+                total = 0
+                plt.cla()
+                ax.plot(theta, distance_list)
+                plt.pause(.1)
+                        
+        except IndexError:
+            ser.write(b'e')
+            print('Stopped! Out of sync.')
         
-
-def animate():
-    r = np.arange(1, 361, 1)
-    theta = np.radians(r)
-    fig = plt.figure()
-    ax = fig.add_subplot(111, polar=True)
-    while True:
-        distances = init();
-        plt.cla()
-        ax.plot(theta, distances)
-        plt.pause(.0001)
-
 try:
     ser.write(b'b')
-    animate()
+    init()
     plt.show()
     
 
