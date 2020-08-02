0d95e82275f1b90a85edc4c5496789f8e781f502
collinpreston <collin_preston@yahoo.com>
2020-08-02 17:30:25 -0400

    Adding debugging output to the screen of the transmitted data.

--- radar.py
+++ radar.py
@@ -40,8 +40,9 @@ try:
             # has not sent a stop command.
             # TODO: Check if the connection is still open and connected to the phone.
             # If not, then we need to keep the pi open and not crash.
-            data = client_sock.recv(1024)
             print('here')
+            data = client_sock.recv(1024)
+
 
             if (data == 'Stop'):
                 # If the phone sends a stop command, then we need
