0313bccdfce3c5f2713db1cfc84d4b1fa2647de6
collinpreston <collin_preston@yahoo.com>
2020-08-02 17:25:11 -0400

    Adding debugging output to the screen of the transmitted data.

--- radar.py
+++ radar.py
@@ -2,64 +2,65 @@ import datetime
 import bluetooth
 import serial
 
-ser = serial.Serial ("/dev/serial0", baudrate=230400)
+ser = serial.Serial("/dev/serial0", baudrate=230400)
 
 try:
-    
+
     # Set the socket type.
-    server_sock = bluetooth.BluetoothSocket( bluetooth.RFCOMM )
-    
+    server_sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+
     # Get an available port on the Raspberry Pi.
-    #port = bluetooth.get_available_port( bluetooth.RFCOMM )
+    # port = bluetooth.get_available_port( bluetooth.RFCOMM )
     port = 1
-    
+
     # Bind the socket server to the Raspberry Pi on the port we got above.
     server_sock.bind(("", port))
-    
+
     # Now we will listen for connection on the port.
     server_sock.listen(1)
-    
+
     # In order to allow for the phone to detect and identify the 
     # correct bluetooth connection, we need to advertise our service.
     UUID = "1e0ca4ea-299d-4335-93eb-27fcfe7fa848"
-    bluetooth.advertise_service( server_sock, "FloorPlanr", UUID )
-    
+    bluetooth.advertise_service(server_sock, "FloorPlanr", UUID)
+
     # Now we can capture the details of the phone once the connection is
     # made.
-    (client_sock,address) = server_sock.accept()
-    
-    print ('Connected to ' + str(address))
-    
+    (client_sock, address) = server_sock.accept()
+
+    print('Connected to ' + str(address))
+
+
     def sendLIDARData():
         distance_list = ""
-        base_angle = -1
         ser.write(b'b')
-    
+
         while True:
             # Here we need to check to make sure that the phone
             # has not sent a stop command.
+            # TODO: Check if the connection is still open and connected to the phone.
+            # If not, then we need to keep the pi open and not crash.
             data = client_sock.recv(1024)
-            
+
             if (data == 'Stop'):
                 # If the phone sends a stop command, then we need
                 # to break the loop and go back to listening for a start
                 # command.
                 break
-            
+
             try:
-                result = ser.read(42)  
+                result = ser.read(42)
                 if (result[-1] == result[-2]):
-                    rpm = result[3]*256 + result[2]
-                    base_angle = (result[1] - 160)*6
+                    rpm = result[3] * 256 + result[2]
+                    base_angle = (result[1] - 160) * 6
                     for x in range(6):
-                        #angle = base_angle + x
-                        distance = result[((6*(x+1))+1)]*256 + result[((6*(x+1)))]
+                        distance = result[((6 * (x + 1)) + 1)] * 256 + result[((6 * (x + 1)))]
                         distance_list = distance_list + "," + distance
-                    
+
                     # After collecting all 6 distances sent from each packet of 
                     # LIDAR data, we will send the distance data along with the
                     # base angle to the phone.
-                    print(str(datetime.datetime.now()) + "_" + str(base_angle) + ":" + str(distance_list) + "#")
+                    print("here")
                     client_sock.send(datetime.datetime.now() + "_" + base_angle + ":" + distance_list + "#")
                 break
             except IndexError:
@@ -73,22 +74,22 @@ try:
         # Here we return with 0 to indicate that the method did not throw any errors.
         # This means that we are returning because the phone sent a stop command.
         return 0
-    
-    
+
+
     lidar_execution_result = 0
 
-# Stay connected while waiting for instructions from the phone.
+    # Stay connected while waiting for instructions from the phone.
     while True:
         data = client_sock.recv(1024).decode()
-        print(data)
-        
+        print(str(data))
+
         # If the sendLIDARData returned with an error, then we need
         # to call the method again.
-        if (data == 'Start' or lidar_execution_result == 1 ):
+        if (data == 'Start' or lidar_execution_result == 1):
             # Here we will call the function to start sending
             # LIDAR data to the phone.
             lidar_execution_result = sendLIDARData()
-        
+
         # TODO: We need to monitor the bluetooth connection.  When the connection is
         # closed, we will need to reset the application (close the connection, 
         # go back to accepting connections).
