4ee681dc8a02fbb84cc252fa70c4a3b3133f0c28
collinpreston <collin_preston@yahoo.com>
2020-08-05 19:29:59 -0400

    Flushing the serial port.

--- radar.py
+++ radar.py
@@ -68,7 +68,7 @@ try:
                     print(str(datetime.datetime.now()) + "_" + str(base_angle) + "*" + str(distance_list) + "#")
 
                     distance_list = ""
-                    ser.flushInput()
+                    ser.flushOutput()
                     time.sleep(.2)
 
             except IndexError:
