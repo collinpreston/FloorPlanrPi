730a0bd819b4fa9b5541a2d99bd958383919af30
collinpreston <collin_preston@yahoo.com>
2020-08-06 06:15:58 -0400

    Little bug.

--- radar.py
+++ radar.py
@@ -79,7 +79,7 @@ try:
             # LIDAR data, we will send the distance data along with the
             # base angle to the phone.
             client_sock.send(supreme_list)
-
+            unique_values = 0
             ser.write(b'e')
             time.sleep(.001)
             ser.write(b'b')
