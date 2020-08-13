0865b19c166b45391f9d18bed2acd8bfd4af69f8
collinpreston <collin_preston@yahoo.com>
2020-08-13 16:46:48 -0400

    Truly working!

--- radar.py
+++ radar.py
@@ -69,14 +69,12 @@ try:
                     return 1
 
             data_group = 0
-            print(str(unique_values))
             while unique_values < 180:
                 result = total_result[(data_group * 42):(42 * data_group) + 42]
                 # TODO: there is the chance that we don't enter the if loop and thus get
                 # stuck in this while loop.  We should have an else condition.
                 print(str(result[-1]) + " " + str(result[-2]))
                 if result[-1] == result[-2]:
-                    print(str(data_group))
                     data_group += 1
                     base_angle = (result[1] - 160) * 6
                     for x in range(6):
@@ -96,7 +94,7 @@ try:
 
             # We need to sleep after sending the stop command to the lidar.  Otherwise the lidar will not turn
             # back on.
-            time.sleep(.05)
+            time.sleep(.005)
 
         # Here we return with 0 to indicate that the method did not throw any errors.
         # This means that we are returning because the phone sent a stop command.
