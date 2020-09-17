38e9864ebce7bbe7b95c4a580b7b8a0e52041ca8
collinpreston <collin_preston@yahoo.com>
2020-09-17 08:53:56 -0400

    Adding debug output.

--- radar.py
+++ radar.py
@@ -69,6 +69,7 @@ try:
     # Stay connected while waiting for instructions from the phone.
     while True:
         data = client_sock.recv(1024).decode()
+        print(data)
 
         # If the sendLIDARData returned with an error, then we need
         # to call the method again.
