a459e28c0da5b02cd0d07b2357d355535f07d6fb
curio-lighthouse <mrplow911@gmail.com>
2020-09-30 17:44:11 -0400

    Updated the script for naming.

--- bluetooth-script.sh
+++ bluetooth-script.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#sudo hciconfig hci0 piscan 
-#sudo hciconfig hci0 sspmode 1
+sudo hciconfig hci0 piscan 
+sudo hciconfig hci0 sspmode 1
 sleep 15
 bluetoothctl -- discoverable on
 bluetoothctl -- pairable on
 bluetoothctl -- agent on
 bluetoothctl -- default-agent
-python3 /home/pi/Downloads/FloorPlanrPi/bluez-test-scripts/examples/simple-agent &
-python3 /home/pi/Downloads/FloorPlanrPi/radar.py
+python3 /home/pi/Downloads/lighthouse/bluez-test-scripts/examples/simple-agent &
+python3 /home/pi/Downloads/lighthouse/lighthouse.py
 
