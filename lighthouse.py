535ecc01c5b228dc762650ca895d221f3661a362
curio-lighthouse <mrplow911@gmail.com>
2020-10-02 10:09:20 -0400

    Added lots of logging output.

--- lighthouse.py
+++ lighthouse.py
@@ -1,9 +1,7 @@
 import bluetooth
 import serial
-import time
 
 ser = serial.Serial("/dev/serial0", baudrate=230400)
-
 
 try:
 
@@ -29,45 +27,58 @@ try:
 
     print("Awaiting connection...")
 
+
     def sendLIDARData(dataPacketSize):
-            ser.write(b'b')
-            while True:
-                try:
-                    data = client_sock.recv(1024).decode()
-                except bluetooth.btcommon.BluetoothError:
-                    data = ""
-                    print("Nothing to read.")
+        print("Starting LiDAR")
+        ser.write(b'b')
+        while True:
+            try:
+                print("reading from bluetooth...")
+                data = client_sock.recv(1024).decode()
+                print("read from bluetooth...")
+            except bluetooth.btcommon.BluetoothError:
+                data = ""
+                print("Nothing to read.")
 
-                if data == 'stop':
-                    # If the phone sends a stop command, then we need
-                    # to break the loop and go back to listening for a start
-                    # command.
-                    return 0
+            if data == 'stop':
+                # If the phone sends a stop command, then we need
+                # to break the loop and go back to listening for a start
+                # command.
+                print("stop command received!")
+                return 0
 
-                try:
-                    result = ser.read(dataPacketSize)
-                    ser.reset_input_buffer()
-                    client_sock.send(result)
+            try:
+                print("reading data from LiDAR...")
+                result = ser.read(dataPacketSize)
+                print("read data from LiDAR.")
+                ser.reset_input_buffer()
 
-                except IndexError:
-                    print('IndexError')
-                    ser.write(b'e')
-                    # Here we will need to go back to the main while loop.
-                    # In the main loop we will check to see if we returned because of
-                    # the LIDAR being out of sync or it the phone sent a stop command.
-                    # We will return 1 to indicate an error.
-                    return 1
-                except bluetooth.btcommon.BluetoothError:
-                    print('Bluetooth disconnected or connection lost...')
-                    ser.write(b'e')
-                    return 2
+                print("sending data to bluetooth...")
+                client_sock.send(result)
+                print("sent data to bluetooth")
+
+            except IndexError:
+                print('IndexError')
+                ser.write(b'e')
+                # Here we will need to go back to the main while loop.
+                # In the main loop we will check to see if we returned because of
+                # the LIDAR being out of sync or it the phone sent a stop command.
+                # We will return 1 to indicate an error.
+                return 1
+            except bluetooth.btcommon.BluetoothError:
+                print('Bluetooth disconnected or connection lost...')
+                ser.write(b'e')
+                return 2
+
 
     lidar_execution_result = 0
 
     # Stay connected while waiting for instructions from the phone.
     while True:
         try:
+            print("reading from bluetooth...")
             data = client_sock.recv(1024).decode()
+            print("read from bluetooth.")
         except bluetooth.btcommon.BluetoothError:
             data = ""
             print("Nothing to read.")
@@ -80,8 +91,8 @@ try:
             lidar_execution_result = sendLIDARData(int(data[5:]))
 
         if lidar_execution_result == 2:
+            print("waiting for bluetooth device to connect...")
             (client_sock, address) = server_sock.accept()
             lidar_execution_result = 0
 except KeyboardInterrupt:
     ser.write(b'e')
-
